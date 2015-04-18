6dadb6cab87ca4a7ccaaecb01877486b79568100
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-18 11:22:38 +0200

    Display screen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import javax.swing.JFrame;
+
 
 public class Main {
+	public static int screenHeight = 1220, screenWidth = 440;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void displayScreen(){
+		Main t = new Main();
+		JFrame jf = new JFrame();
+		jf.setTitle("An Unconventional Weapon"); //Displaying the title on the frame
+		jf.setSize(screenHeight, screenWidth); //Setting size of frame (x,y);
+		jf.setVisible(true); //Display the frame.
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	}
-
+	public static void main(String[] args) {
+		displayScreen();
+	}
 }
